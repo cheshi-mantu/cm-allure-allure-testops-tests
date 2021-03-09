7e05d80906b3452b012864334a0f79c908513e28
Cheshi Mantu <cheshi.mantu@protonmail.com>
2021-03-10 00:30:43 +0300

    updates fake API tests

--- src/test/java/xyz/egorivanov/tests/SuccessOrFailOrBreakAPITests.java
+++ src/test/java/xyz/egorivanov/tests/SuccessOrFailOrBreakAPITests.java
@@ -1,12 +1,14 @@
 package xyz.egorivanov.tests;
 
-import xyz.egorivanov.examples.Layer;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
+import xyz.egorivanov.examples.Layer;
+import xyz.egorivanov.examples.Microservice;
 
 import static helpers.AttachmentsHelper.attachAsText;
-
 import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
 
@@ -14,45 +16,48 @@ import static io.qameta.allure.Allure.step;
 @Feature("Elements")
 @Tag("various_tests")
 @Owner("egorivanov")
-class SuccessOrFailOrBreakTests extends TestBase {
+class SuccessOrFailOrBreakAPITests extends TestBase {
 
     @Test
-    @Layer("web")
-    @DisplayName("This test throws AssertionFailedError sometimes")
-    @Description("If test is to fail then throw an exception, otherwise don't")
+    @Layer("API")
+    @Microservice("billing")
+    @DisplayName("Fake API test with possible assertion error")
+    @Description("We get isTestAFailure from @BeforeEach method and throw assertion erorr if less than 1")
     @Severity(SeverityLevel.TRIVIAL)
     public void throwingAssertionFailedError() {
         parameter("ReasonToLive", isTestAFailure);
-        step ("Check if test needs to fail, then fail", () -> {
+        step ("Storing the text attachment", () -> {
             attachAsText("isTestAFailure",isTestAFailure+"");
-            if (isTestAFailure < 2 ) {
+            if (isTestAFailure < 1 ) {
                 throw new AssertionFailedError("Assertion failed error, expected one but got two!", 1, isTestAFailure);
             }
         });
     }
 
     @Test
-    @Layer("web")
-    @DisplayName("Assert generated value against 2 and throw exception")
-    @Description("Generated random int is tested against 3 and if less then test will fail")
+    @Layer("API")
+    @Microservice("store_order")
+    @DisplayName("Assert generated value against 1 and throw exception if less")
+    @Description("Get random value from @BeforeEach and then compare with 1, if less then throw assertion error")
     @Severity(SeverityLevel.NORMAL)
     public void assertEqualsWithFailTest() {
         parameter("ReasonToLive", isTestAFailure);
-        step ("Check if test needs to fail, then fail", () -> {
+        step ("Storing the text attachment", () -> {
             attachAsText("isTestAFailure",isTestAFailure+"");
-            if (isTestAFailure < 2 ) {
+            if (isTestAFailure < 1 ) {
                 throw new AssertionFailedError("Assertion failed error, expected one but got the other!", ">3", isTestAFailure);
             }
         });
     }
     @Test
-    @Layer("web")
+    @Layer("API")
+    @Microservice("store_order")
     @DisplayName("Assert generated value against 1 and throw runtime exception if less")
-    @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
+    @Description("Get random value from @BeforeEach and then compare with 1, if less then throw runtime exception error")
     @Severity(SeverityLevel.CRITICAL)
     public void compareGeneratedWithFixedAndThrowRuntimeExceptionTest() {
         parameter("ReasonToLive", isTestAFailure);
-        step ("Check if test needs to fail, then fail", () -> {
+        step ("Storing the text attachement", () -> {
             attachAsText("isTestAFailure",isTestAFailure+"");
             if (isTestAFailure < 1 ) {
                 throw new RuntimeException("Reason to live not found");
@@ -60,14 +65,15 @@ class SuccessOrFailOrBreakTests extends TestBase {
         });
     }
     @Test
-    @Layer("web")
-    @DisplayName("Assert generated value against 5 and throw runtime exception if less")
-    @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
+    @Layer("API")
+    @Microservice("process_order")
+    @DisplayName("Assert generated value against 1 and throw runtime exception if less")
+    @Description("Generated random int is tested against 5, this test will be a flaky one")
     @Severity(SeverityLevel.MINOR)
     @Flaky
     public void flakyRandomizedTest() {
         parameter("ReasonToLive", isTestAFailure);
-        step ("Check if test needs to fail, then fail", () -> {
+        step ("Storing the text attachment", () -> {
             attachAsText("isTestAFailure",isTestAFailure+"");
         });
         step ("Check if test needs to fail, then fail", () -> {
@@ -75,7 +81,8 @@ class SuccessOrFailOrBreakTests extends TestBase {
         });
     }
     @Test
-    @Layer("web")
+    @Layer("API")
+    @Microservice("process_order")
     @DisplayName("This test will always pass as we'll trick the assertion")
     @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
     @Severity(SeverityLevel.MINOR)
---
+++ src/test/java/xyz/egorivanov/tests/SuccessOrFailOrBreakWebTests.java
@@ -0,0 +1,94 @@
+package xyz.egorivanov.tests;
+
+import xyz.egorivanov.examples.Layer;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.*;
+import org.opentest4j.AssertionFailedError;
+
+import static helpers.AttachmentsHelper.attachAsText;
+
+import static io.qameta.allure.Allure.parameter;
+import static io.qameta.allure.Allure.step;
+
+@Epic("Allure TestOps demo tests")
+@Feature("Elements")
+@Tag("various_tests")
+@Owner("egorivanov")
+class SuccessOrFailOrBreakWebTests extends TestBase {
+
+    @Test
+    @Layer("web")
+    @DisplayName("This test throws AssertionFailedError sometimes")
+    @Description("If test is to fail then throw an exception, otherwise don't")
+    @Severity(SeverityLevel.TRIVIAL)
+    public void throwingAssertionFailedError() {
+        parameter("ReasonToLive", isTestAFailure);
+        step ("Check if test needs to fail, then fail", () -> {
+            attachAsText("isTestAFailure",isTestAFailure+"");
+            if (isTestAFailure < 2 ) {
+                throw new AssertionFailedError("Assertion failed error, expected one but got two!", 1, isTestAFailure);
+            }
+        });
+    }
+
+    @Test
+    @Layer("web")
+    @DisplayName("Assert generated value against 2 and throw exception")
+    @Description("Generated random int is tested against 3 and if less then test will fail")
+    @Severity(SeverityLevel.NORMAL)
+    public void assertEqualsWithFailTest() {
+        parameter("ReasonToLive", isTestAFailure);
+        step ("Check if test needs to fail, then fail", () -> {
+            attachAsText("isTestAFailure",isTestAFailure+"");
+            if (isTestAFailure < 2 ) {
+                throw new AssertionFailedError("Assertion failed error, expected one but got the other!", ">3", isTestAFailure);
+            }
+        });
+    }
+    @Test
+    @Layer("web")
+    @DisplayName("Assert generated value against 1 and throw runtime exception if less")
+    @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
+    @Severity(SeverityLevel.CRITICAL)
+    public void compareGeneratedWithFixedAndThrowRuntimeExceptionTest() {
+        parameter("ReasonToLive", isTestAFailure);
+        step ("Check if test needs to fail, then fail", () -> {
+            attachAsText("isTestAFailure",isTestAFailure+"");
+            if (isTestAFailure < 1 ) {
+                throw new RuntimeException("Reason to live not found");
+            }
+        });
+    }
+    @Test
+    @Layer("web")
+    @DisplayName("Assert generated value against 5 and throw runtime exception if less")
+    @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
+    @Severity(SeverityLevel.MINOR)
+    @Flaky
+    public void flakyRandomizedTest() {
+        parameter("ReasonToLive", isTestAFailure);
+        step ("Check if test needs to fail, then fail", () -> {
+            attachAsText("isTestAFailure",isTestAFailure+"");
+        });
+        step ("Check if test needs to fail, then fail", () -> {
+            assert(isTestAFailure>5);
+        });
+    }
+    @Test
+    @Layer("web")
+    @DisplayName("This test will always pass as we'll trick the assertion")
+    @Description("Generated random int is tested against 3 and if less then runtime exception will be thrown")
+    @Severity(SeverityLevel.MINOR)
+    @Flaky
+    public void goodTestWhichPasses() {
+        parameter("ReasonToLive", isTestAFailure);
+        step ("Attaching isTestAFailure as text to the test results", () -> {
+            attachAsText("isTestAFailure",isTestAFailure+"");
+
+        });
+        step ("Assertion will always pass", () -> {
+            assert(isTestAFailure<11);
+        });
+    }
+
+}
