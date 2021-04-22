9965320163367f8f0e119ad37328fbe06f4d34ac
Egor Ivanov <cheshi.mantu@pm.me>
2021-04-22 17:06:58 +0300

    upgraded allure java version

--- src/test/java/xyz/egorivanov/tests/ManualToAutomatedExampleTests.java
+++ src/test/java/xyz/egorivanov/tests/ManualToAutomatedExampleTests.java
@@ -1,16 +1,9 @@
 package xyz.egorivanov.tests;
 
 import io.qameta.allure.*;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import xyz.egorivanov.examples.Layer;
-import xyz.egorivanov.examples.Lead;
-import xyz.egorivanov.examples.Microservice;
 
-import static helpers.AttachmentsHelper.attachAsText;
-import static io.qameta.allure.Allure.parameter;
-import static io.qameta.allure.Allure.step;
 
 @Epic("Allure TestOps demo tests")
 @Feature("Automation of existing manual tests")
